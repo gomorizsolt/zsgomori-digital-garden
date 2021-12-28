774cd90846a9d57f6cce70257214efc2970477f1
Zsolt Gomori <zsgomoridev@icloud.com>
2021-12-28 14:41:51 +0100

    remove test content, add button

---
+++ components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    class="flex items-center p-2 font-medium text-white transition-colors duration-200 bg-slate-900 rounded-md hover:bg-slate-800 focus:outline-none focus:ring focus:ring-slate-300 focus:ring-opacity-80"
+  >
+    <slot name="prefixIcon"></slot>
+
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>
