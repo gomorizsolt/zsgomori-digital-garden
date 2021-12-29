167df7af4d813e9fccf70a5f86a72b8955d1d7ee
Zsolt Gomori <zsgomoridev@icloud.com>
2021-12-29 07:44:57 +0100

    disable lazy loading of back button

--- layouts/note.vue
+++ layouts/note.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="prose prose-sm md:prose mx-auto py-10 px-6">
     <NuxtLink to="/" class="mb-6 block">
-      <LazyButton aria-label="Back">
+      <Button aria-label="Back">
         <template #prefixIcon>
-          <LazyArrowCircleLeft aria-hidden="true" />
+          <ArrowCircleLeft aria-hidden="true" />
         </template>
-      </LazyButton>
+      </Button>
     </NuxtLink>
 
     <Nuxt />
   </div>
 </template>
-
-<script>
-export default {};
-</script>
